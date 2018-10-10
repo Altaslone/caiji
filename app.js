b41dbc81891c437ad1c7595c5c1c9d91813dca6a
ulogic <278944629@139.com>
2018-10-10 17:31:08 +0800

    接口部分

--- app.js
+++ app.js
@@ -1,39 +1,86 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    onLaunch: function() {
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+        if (wx.getStorageSync('openId')) {
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+        } else {
+            // 登录
+            wx.login({
+                success: res => {
+                    // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                    wx.request({
+                        url: 'https://caiji.dahang.xyz',
+                        header: {
+                            'content-type': 'application/x-www-form-urlencoded',
+                            'caiji': 'v0.1'
+                        },
+                        data: {
+                            req: 'login',
+                            code: res.code
+                        },
+                        method: 'POST',
+                        dataType: 'json',
+                        responseType: 'text',
+                        success: function(r) {
+
+                            let data = r.data.msg;
+                            if (r.data.status == 'ok') {
+
+                                wx.setStorageSync('uid', data.id);
+                                wx.setStorageSync('nickName', data.name);
+                                wx.setStorageSync('avatarUrl', data.avatar);
+                                wx.setStorageSync('openId', data.openId);
+                                wx.setStorageSync('unionId', data.unionId);
+                                wx.setStorageSync('gender', data.gender);
+
+                            } else {
+                                wx.showToast({
+                                    title: '未知异常!',
+                                    icon: icon,
+                                    duration: 3000
+                                });
+                            }
+                        },
+                        fail: function(r) {
+                            console.log('fail');
+                            wx.showToast({
+                                title: '未知异常!',
+                                icon: icon,
+                                duration: 3000
+                            });
+                        },
+                        complete: function(r) {
+
+                        }
+                    });
+                }
+            });
+
+            // 获取用户信息
+            wx.getSetting({
+                success: res => {
+                    if (res.authSetting['scope.userInfo']) {
+                        // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                        wx.getUserInfo({
+                            success: res => {
+                                // 可以将 res 发送给后台解码出 unionId
+                                // this.globalData.userInfo = res.userInfo;
+
+                                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                                // 所以此处加入 callback 以防止这种情况
+                                if (this.userInfoReadyCallback) {
+                                    this.userInfoReadyCallback(res);
+                                }
+                            }
+                        })
+                    }
+                }
+            });
+
         }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+    },
+    globalData: {
+
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -9,65 +9,101 @@ Page({
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo')
     },
-    //事件处理函数
-    bindViewTap: function() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
-    },
     onLoad: function() {
-        if (app.globalData.userInfo) {
+
+        if (wx.getStorageSync('nickName')) {
             this.setData({
-                userInfo: app.globalData.userInfo,
+                userInfo: {
+                    nickName: wx.getStorageSync('nickName'),
+                    avatarUrl: wx.getStorageSync('avatarUrl')
+                },
                 hasUserInfo: true
-            })
+            });
+
         } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回 所以此处加入 callback 以防止这种情况
             app.userInfoReadyCallback = res => {
                 this.setData({
-                    userInfo: res.userInfo,
+                    userInfo: {
+                        nickName: wx.getStorageSync('nickName'),
+                        avatarUrl: wx.getStorageSync('avatarUrl')
+                    },
                     hasUserInfo: true
-                })
+                });
             }
+
         } else {
             // 在没有 open-type=getUserInfo 版本的兼容处理
             wx.getUserInfo({
                 success: res => {
-                    app.globalData.userInfo = res.userInfo
                     this.setData({
-                        userInfo: res.userInfo,
+                        userInfo: {
+                            nickName: wx.getStorageSync('nickName'),
+                            avatarUrl: wx.getStorageSync('avatarUrl')
+                        },
                         hasUserInfo: true
-                    })
+                    });
                 }
-            })
+            });
         }
     },
     getUserInfo: function(e) {
-        console.log(e)
-        app.globalData.userInfo = e.detail.userInfo
+        let userInfo = e.detail.userInfo;
+        if (userInfo.avatarUrl != wx.getStorageSync('avatarUrl')) {
+            wx.setStorageSync('avatarUrl', userInfo.avatarUrl);
+        }
+
+        if (userInfo.nickName != wx.getStorageSync('nickName')) {
+            wx.setStorageSync('nickName', userInfo.nickName);
+        }
+
+        if (userInfo.gender != wx.getStorageSync('gender')) {
+            wx.setStorageSync('gender', userInfo.gender);
+        }
+
         this.setData({
-            userInfo: e.detail.userInfo,
+            userInfo: {
+                nickName: wx.getStorageSync('nickName'),
+                avatarUrl: wx.getStorageSync('avatarUrl')
+            },
             hasUserInfo: true
-        })
+        });
+
+        wx.request({
+            url: 'https://caiji.dahang.xyz',
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'caiji': 'v0.1'
+            },
+            data: {
+                req: 'updateUserInfo',
+                id: wx.getStorageSync('uid'),
+                openId: wx.getStorageSync('openId'),
+                name: userInfo.nickName,
+                avatar: userInfo.avatarUrl,
+                gender: userInfo.gender
+            },
+            method: 'POST',
+            dataType: 'json',
+            responseType: 'text',
+            success: function(r) {},
+            fail: function(r) {},
+            complete: function(r) {}
+        });
     },
     writeCaiJi: function() {
         wx.navigateTo({
             url: '/pages/writeCaiJi/writeCaiJi',
-            success: function(res) {
-                console.log(res);
-            },
+            success: function(res) {},
             fail: function(res) {
                 console.log(res);
                 wx.showModal({
                     title: '异常',
                     content: '未知异常',
                     showCancel: false
-                })
+                });
             },
-            complete: function(res) {
-                console.log(res);
-            },
+            complete: function(res) {},
         })
     }
 })
